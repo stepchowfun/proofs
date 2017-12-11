2970a84aa9fec418795152bb34343e79338371d6
Stephan Boyer <stephan@stephanboyer.com>
2017-12-11 03:43:09 -0800

    Prove that categories form a category

---
+++ coq/CategoryTheory/Cat.v
@@ -0,0 +1,41 @@
+(*****************************************************)
+(*****************************************************)
+(****                                             ****)
+(****   A proof that categories form a category   ****)
+(****                                             ****)
+(*****************************************************)
+(*****************************************************)
+
+Require Import Coq.Logic.FunctionalExtensionality.
+Require Import Coq.Logic.ProofIrrelevance.
+Require Import Main.CategoryTheory.Category.
+Require Import Main.CategoryTheory.Functor.
+Require Import Main.Tactics.
+
+Definition catCategory : category.
+Proof.
+  refine (
+    newCategory
+    category
+    (fun x y => @functor x y)
+    (fun x y z => compFunctor)
+    (fun x => idFunctor)
+    _ _
+  ); intros.
+  - destruct f.
+    destruct g.
+    destruct h.
+    unfold compFunctor.
+    cbn.
+    f_equal.
+    apply proof_irrelevance.
+    do 5 (apply functional_extensionality_dep; intros).
+    apply proof_irrelevance.
+  - split;
+      intros;
+      destruct f;
+      unfold compFunctor;
+      cbn;
+      f_equal;
+      apply proof_irrelevance.
+Defined.
