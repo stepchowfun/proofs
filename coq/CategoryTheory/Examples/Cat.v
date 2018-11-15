9fb6a60182b26ed4b2f4c2004a44ff502b85a2b6
Stephan Boyer <stephan@stephanboyer.com>
2018-11-15 02:45:29 -0800

    Don't add axioms to the global hints database

--- coq/CategoryTheory/Examples/Cat.v
+++ coq/CategoryTheory/Examples/Cat.v
@@ -9,15 +9,20 @@
 Require Import Main.CategoryTheory.Category.
 Require Import Main.CategoryTheory.Functor.
 Require Import Main.Tactics.
+Require Import ProofIrrelevance.
 
-Definition catCategory : category.
-Proof.
-  refine (
-    newCategory
-    category
-    (fun x y => @functor x y)
-    (fun x y z => compFunctor)
-    (fun x => idFunctor)
-    _ _ _
-  ); unfold compFunctor; destruct f; magic.
-Defined.
+Section ProofIrrelevance.
+  Hint Resolve proof_irrelevance.
+
+  Definition catCategory : category.
+  Proof.
+    refine (
+      newCategory
+      category
+      (fun x y => @functor x y)
+      (fun x y z => compFunctor)
+      (fun x => idFunctor)
+      _ _ _
+    ); unfold compFunctor; destruct f; magic.
+  Defined.
+End ProofIrrelevance.
