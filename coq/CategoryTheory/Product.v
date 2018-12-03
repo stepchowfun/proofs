c1bc7aa749a63be5eceb20ad83d7d8a0932e0620
Stephan Boyer <stephan@stephanboyer.com>
2018-12-02 19:24:59 -0800

    Prove that products and coproducts are associative up to isomorphism

--- coq/CategoryTheory/Product.v
+++ coq/CategoryTheory/Product.v
@@ -86,3 +86,178 @@ Proof.
 Qed.
 
 Hint Resolve productCommutative.
+
+Theorem productAssociative
+  {C : category}
+  (x y z xy yz xy_z x_yz : object C)
+  (xy_to_x : arrow C xy x)
+  (xy_to_y : arrow C xy y)
+  (yz_to_y : arrow C yz y)
+  (yz_to_z : arrow C yz z)
+  (xy_z_to_xy : arrow C xy_z xy)
+  (xy_z_to_z : arrow C xy_z z)
+  (x_yz_to_x : arrow C x_yz x)
+  (x_yz_to_yz : arrow C x_yz yz)
+: product x y xy xy_to_x xy_to_y ->
+  product y z yz yz_to_y yz_to_z ->
+  product xy z xy_z xy_z_to_xy xy_z_to_z ->
+  product x yz x_yz x_yz_to_x x_yz_to_yz ->
+  isomorphic xy_z x_yz.
+Proof.
+  unfold product.
+  clean.
+
+  Ltac instantiateUniversal H :=
+    unfold universal in H;
+    unfold arrowExists in H;
+    do 3 destruct H;
+    sort.
+
+  fact (H x_yz x_yz_to_x (compose C yz_to_y x_yz_to_yz)).
+  instantiateUniversal H3.
+  rename x0 into x_yz_to_xy.
+  clear H4.
+
+  fact (H0 xy_z (compose C xy_to_y xy_z_to_xy) xy_z_to_z).
+  instantiateUniversal H4.
+  rename x0 into xy_z_to_yz.
+  clear H6.
+
+  fact (H2 xy_z (compose C xy_to_x xy_z_to_xy) xy_z_to_yz).
+  instantiateUniversal H6.
+  rename x0 into xy_z_to_x_yz.
+  clear H8.
+
+  fact (H1 x_yz x_yz_to_xy (compose C yz_to_z x_yz_to_yz)).
+  instantiateUniversal H8.
+  rename x0 into x_yz_to_xy_z.
+  clear H10.
+
+  assert (id C = compose C x_yz_to_xy_z xy_z_to_x_yz).
+  - assert (
+      compose C xy_to_y (
+        compose C xy_z_to_xy (compose C x_yz_to_xy_z xy_z_to_x_yz)
+      ) = compose C xy_to_y xy_z_to_xy
+    ).
+    + replace (compose C xy_z_to_xy (compose C x_yz_to_xy_z xy_z_to_x_yz))
+        with (compose C (compose C xy_z_to_xy x_yz_to_xy_z) xy_z_to_x_yz);
+          magic.
+      rewrite <- H8.
+      rewrite cAssoc.
+      rewrite <- H5.
+      rewrite <- cAssoc.
+      magic.
+    + assert (
+        compose C xy_to_x (
+          compose C xy_z_to_xy (compose C x_yz_to_xy_z xy_z_to_x_yz)
+        ) = compose C xy_to_x xy_z_to_xy
+      ).
+      * replace (compose C xy_z_to_xy (compose C x_yz_to_xy_z xy_z_to_x_yz))
+          with (compose C (compose C xy_z_to_xy x_yz_to_xy_z) xy_z_to_x_yz);
+            magic.
+        rewrite <- H8.
+        rewrite cAssoc.
+        magic.
+      * {
+        fact (
+          H
+          xy_z
+          (
+            compose C xy_to_x (
+              compose C xy_z_to_xy (compose C x_yz_to_xy_z xy_z_to_x_yz)
+            )
+          )
+          (
+            compose C xy_to_y (
+              compose C xy_z_to_xy (compose C x_yz_to_xy_z xy_z_to_x_yz)
+            )
+          )
+        ).
+        unfold universal in H13.
+        destruct H13.
+        clear H13.
+        unfold arrowUnique in H14.
+        specialize (
+          H14 xy_z_to_xy (
+            compose C xy_z_to_xy (compose C x_yz_to_xy_z xy_z_to_x_yz)
+          )
+        ).
+        do 2 feed H14.
+
+        assert (
+          compose C xy_z_to_z (compose C x_yz_to_xy_z xy_z_to_x_yz) = xy_z_to_z
+        ); [rewrite cAssoc; rewrite <- H11; magic | idtac].
+
+        fact (H1 xy_z xy_z_to_xy xy_z_to_z).
+        unfold universal in H15.
+        magic.
+      }
+  - assert (id C = compose C xy_z_to_x_yz x_yz_to_xy_z).
+    + assert (
+        compose C yz_to_y (
+          compose C x_yz_to_yz (compose C xy_z_to_x_yz x_yz_to_xy_z)
+        ) = compose C yz_to_y x_yz_to_yz
+      ).
+      * replace (compose C x_yz_to_yz (compose C xy_z_to_x_yz x_yz_to_xy_z))
+          with (compose C (compose C x_yz_to_yz xy_z_to_x_yz) x_yz_to_xy_z);
+            magic.
+        rewrite <- H9.
+        rewrite cAssoc.
+        rewrite <- H4.
+        rewrite <- cAssoc.
+        magic.
+      * {
+        assert (
+          compose C yz_to_z (
+            compose C x_yz_to_yz (compose C xy_z_to_x_yz x_yz_to_xy_z)
+          ) = compose C yz_to_z x_yz_to_yz
+        ).
+        - replace (compose C x_yz_to_yz (compose C xy_z_to_x_yz x_yz_to_xy_z))
+            with (compose C (compose C x_yz_to_yz xy_z_to_x_yz) x_yz_to_xy_z);
+              magic.
+          rewrite <- H9.
+          rewrite cAssoc.
+          magic.
+        - fact (
+            H0
+            x_yz
+            (
+              compose C yz_to_y (
+                compose C x_yz_to_yz (compose C xy_z_to_x_yz x_yz_to_xy_z)
+              )
+            )
+            (
+              compose C yz_to_z (
+                compose C x_yz_to_yz (compose C xy_z_to_x_yz x_yz_to_xy_z)
+              )
+            )
+          ).
+          unfold universal in H14.
+          destruct H14.
+          clear H14.
+          unfold arrowUnique in H15.
+          specialize (
+            H15 x_yz_to_yz (
+              compose C x_yz_to_yz (compose C xy_z_to_x_yz x_yz_to_xy_z)
+            )
+          ).
+          do 2 feed H15.
+
+          assert (
+            compose C x_yz_to_x (compose C xy_z_to_x_yz x_yz_to_xy_z) =
+              x_yz_to_x
+          ); [rewrite cAssoc; rewrite <- H6; magic | idtac].
+
+          fact (H2 x_yz x_yz_to_x x_yz_to_yz).
+          unfold universal in H16.
+          magic.
+      }
+    + unfold isomorphic.
+      exists xy_z_to_x_yz.
+      unfold isomorphism.
+      exists x_yz_to_xy_z.
+      unfold inverse.
+      magic.
+Qed.
+
+Hint Resolve productAssociative.
