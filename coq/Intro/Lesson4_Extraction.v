5fab08c4b957b0f2e5222299d8403c78964a8540
Stephan Boyer <stephan@stephanboyer.com>
2018-12-11 13:52:36 -0800

    Add an example of extraction to Haskell

---
+++ coq/Intro/Lesson4_Extraction.v
@@ -0,0 +1,55 @@
+(**************************************)
+(**************************************)
+(****                              ****)
+(****   Extracting verified code   ****)
+(****                              ****)
+(**************************************)
+(**************************************)
+
+Require Import Extraction.
+Require Import Nat.
+Require Import Omega.
+
+Extraction Language Haskell.
+
+(*
+  For simple enumerated data types, we can simply give the translation of the
+  type and its constructors.
+*)
+
+Extract Inductive bool => "Bool" [ "True" "False" ].
+
+(*
+  For inductive data types for which the arguments have constructors, we must
+  also provide a function for pattern matching.
+*)
+
+Extract Inductive nat =>
+  "Integer"
+  [ "0" "(+1)" ]
+  "(\zero succ n -> if n == 0 then zero () else succ (n - 1))".
+
+(* We can also give translations for constants. *)
+
+Extract Constant plus => "(+)".
+Extract Constant mult => "(*)".
+
+(*
+  Let's write a simple program with dependent types, prove it correct, and
+  extract the computational part to Haskell.
+*)
+
+Definition double (x : nat) : {y : nat | even y = true }.
+Proof.
+  exists (x * 2).
+  induction x; auto.
+Defined.
+
+Theorem double_correct : forall x, proj1_sig (double x) = x + x.
+Proof.
+  intro.
+  cbn.
+  omega.
+Qed.
+
+Recursive Extraction double.
