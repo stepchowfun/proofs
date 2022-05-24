5f222b557458f973574028ae35b58cf5dfee807f
Stephan Boyer <stephan@stephanboyer.com>
2022-05-24 05:00:18 -0700

    Refactor the `Indices` module

--- proofs/TypeTheory/Indices.v
+++ proofs/TypeTheory/Indices.v
@@ -13,6 +13,12 @@
 Require Import Coq.Init.Nat.
 Require Import Main.Tactics.
 
+(*
+  We define two inductive definitions and prove they are isomorphic. The first
+  definition is an inductive family, and the second is a family of inductive
+  types.
+*)
+
 Inductive exp1 : Set -> Type :=
 | const1 : forall (a : Set), a -> exp1 a
 | add1 : exp1 nat -> exp1 nat -> exp1 nat
@@ -26,26 +32,6 @@ Inductive exp2 (a : Set) : Type :=
 | lessThan2 : bool = a -> exp2 nat -> exp2 nat -> exp2 a.
 
 #[export] Hint Constructors exp2 : main.
-
-Fixpoint eval1 (a : Set) (e1 : exp1 a) : a :=
-  match e1 in exp1 b return b with
-  | const1 _ x => x
-  | add1 e2 e3 => eval1 nat e2 + eval1 nat e3
-  | lessThan1 e2 e3 => ltb (eval1 nat e2) (eval1 nat e3)
-  end.
-
-Fixpoint eval2 (a : Set) (e1 : exp2 a) : a :=
-  match e1 with
-  | const2 _ x => x
-  | add2 _ H e2 e3 =>
-    match H in (_ = b) return b with
-    | eq_refl => eval2 nat e2 + eval2 nat e3
-    end
-  | lessThan2 _ H e2 e3 =>
-    match H in (_ = b) return b with
-    | eq_refl => ltb (eval2 nat e2) (eval2 nat e3)
-    end
-  end.
 
 Fixpoint exp1ToExp2 (a : Set) (e1 : exp1 a) : exp2 a :=
   match e1 with
@@ -68,7 +54,50 @@ Fixpoint exp2ToExp1 (a : Set) (e1 : exp2 a) : exp1 a :=
     end
   end.
 
-Theorem exp1ToExp2Homomorphism :
+Theorem exp1ToExp2ToExp1 :
+  forall (a : Set) (e : exp1 a), exp2ToExp1 a (exp1ToExp2 a e) = e.
+Proof.
+  clean.
+  induction e; magic.
+Qed.
+
+#[export] Hint Resolve exp1ToExp2ToExp1 : main.
+
+Theorem exp2ToExp1ToExp2 :
+  forall (a : Set) (e : exp2 a), exp1ToExp2 a (exp2ToExp1 a e) = e.
+Proof.
+  clean.
+  induction e; magic.
+Qed.
+
+#[export] Hint Resolve exp2ToExp1ToExp2 : main.
+
+(*
+  Just for fun, we implement evaluators for both of the inductive definitions
+  and prove they are preserved by the isomorphisms.
+*)
+
+Fixpoint eval1 (a : Set) (e1 : exp1 a) : a :=
+  match e1 in exp1 b return b with
+  | const1 _ x => x
+  | add1 e2 e3 => eval1 nat e2 + eval1 nat e3
+  | lessThan1 e2 e3 => ltb (eval1 nat e2) (eval1 nat e3)
+  end.
+
+Fixpoint eval2 (a : Set) (e1 : exp2 a) : a :=
+  match e1 with
+  | const2 _ x => x
+  | add2 _ H e2 e3 =>
+    match H in (_ = b) return b with
+    | eq_refl => eval2 nat e2 + eval2 nat e3
+    end
+  | lessThan2 _ H e2 e3 =>
+    match H in (_ = b) return b with
+    | eq_refl => ltb (eval2 nat e2) (eval2 nat e3)
+    end
+  end.
+
+Theorem exp1ToExp2PreservesEval :
   forall (a : Set) (e : exp1 a), eval1 a e = eval2 a (exp1ToExp2 a e).
 Proof.
   clean.
@@ -79,9 +108,9 @@ Proof.
   magic.
 Qed.
 
-#[export] Hint Resolve exp1ToExp2Homomorphism : main.
+#[export] Hint Resolve exp1ToExp2PreservesEval : main.
 
-Theorem exp2ToExp1Homomorphism :
+Theorem exp2ToExp1PreservesEval :
   forall (a : Set) (e : exp2 a), eval2 a e = eval1 a (exp2ToExp1 a e).
 Proof.
   clean.
@@ -92,22 +121,4 @@ Proof.
   magic.
 Qed.
 
-#[export] Hint Resolve exp2ToExp1Homomorphism : main.
-
-Theorem exp1ToExp2Isomorphism :
-  forall (a : Set) (e : exp1 a), exp2ToExp1 a (exp1ToExp2 a e) = e.
-Proof.
-  clean.
-  induction e; magic.
-Qed.
-
-#[export] Hint Resolve exp1ToExp2Isomorphism : main.
-
-Theorem exp2ToExp1Isomorphism :
-  forall (a : Set) (e : exp2 a), exp1ToExp2 a (exp2ToExp1 a e) = e.
-Proof.
-  clean.
-  induction e; magic.
-Qed.
-
-#[export] Hint Resolve exp2ToExp1Isomorphism : main.
+#[export] Hint Resolve exp2ToExp1PreservesEval : main.
