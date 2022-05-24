3c58782895632e7364d17dc93097b7983df3ae7a
Stephan Boyer <stephan@stephanboyer.com>
2022-05-24 02:52:47 -0700

    Reformulate the `Indices` module

--- proofs/TypeTheory/Indices.v
+++ proofs/TypeTheory/Indices.v
@@ -10,75 +10,57 @@
 (**************************************************************************)
 (**************************************************************************)
 
+Require Import Coq.Init.Nat.
 Require Import Main.Tactics.
 
 Inductive exp1 : Set -> Type :=
-| zero1 : exp1 nat
-| succ1 : exp1 nat -> exp1 nat
-| pair1 : forall b c, exp1 b -> exp1 c -> exp1 (prod b c).
+| const1 : forall (a : Set), a -> exp1 a
+| add1 : exp1 nat -> exp1 nat -> exp1 nat
+| lessThan1 : exp1 nat -> exp1 nat -> exp1 bool.
+
+Inductive exp2 (a : Set) : Type :=
+| const2 : a -> exp2 a
+| add2 : nat = a -> exp2 nat -> exp2 nat -> exp2 a
+| lessThan2 : bool = a -> exp2 nat -> exp2 nat -> exp2 a.
 
 Fixpoint eval1 (a : Set) (e1 : exp1 a) : a :=
   match e1 in exp1 b return b with
-  | zero1 => 0
-  | succ1 e2 => eval1 nat e2 + 1
-  | pair1 c d e2 e3 => (eval1 c e2, eval1 d e3)
+  | const1 _ x => x
+  | add1 e2 e3 => eval1 nat e2 + eval1 nat e3
+  | lessThan1 e2 e3 => ltb (eval1 nat e2) (eval1 nat e3)
   end.
-
-Inductive exp2 (a : Set) : Type :=
-| zero2 : nat = a -> exp2 a
-| succ2 : nat = a -> exp2 a -> exp2 a
-| pair2 : forall (b c : Set), (prod b c) = a -> exp2 b -> exp2 c -> exp2 a.
 
 Fixpoint eval2 (a : Set) (e1 : exp2 a) : a :=
   match e1 with
-  | zero2 _ H =>
+  | const2 _ x => x
+  | add2 _ H e2 e3 =>
     match H in (_ = b) return b with
-    | eq_refl => 0
+    | eq_refl => eval2 nat e2 + eval2 nat e3
     end
-  | succ2 _ H e2 =>
+  | lessThan2 _ H e2 e3 =>
     match H in (_ = b) return b with
-    | eq_refl =>
-      eval2 nat (
-        match (eq_sym H) in (_ = c) return exp2 c with
-        | eq_refl => e2
-        end
-      ) + 1
-    end
-  | pair2 _ b c H e2 e3 =>
-    match H in (_ = b) return b with
-    | eq_refl => (eval2 b e2, eval2 c e3)
+    | eq_refl => ltb (eval2 nat e2) (eval2 nat e3)
     end
   end.
 
 Fixpoint exp1ToExp2 (a : Set) (e1 : exp1 a) : exp2 a :=
   match e1 with
-  | zero1 => zero2 nat eq_refl
-  | succ1 e2 => succ2 nat eq_refl (exp1ToExp2 nat e2)
-  | pair1 c d e2 e3 =>
-    pair2 (prod c d) c d eq_refl (exp1ToExp2 c e2) (exp1ToExp2 d e3)
+  | const1 b x => const2 b x
+  | add1 e2 e3 => add2 nat eq_refl (exp1ToExp2 nat e2) (exp1ToExp2 nat e3)
+  | lessThan1 e2 e3 =>
+    lessThan2 bool eq_refl (exp1ToExp2 nat e2) (exp1ToExp2 nat e3)
   end.
 
 Fixpoint exp2ToExp1 (a : Set) (e1 : exp2 a) : exp1 a :=
   match e1 with
-  | zero2 _ H =>
+  | const2 _ x => const1 a x
+  | add2 _ H e2 e3 =>
     match H in (_ = b) return exp1 b with
-    | eq_refl => zero1
+    | eq_refl => add1 (exp2ToExp1 nat e2) (exp2ToExp1 nat e3)
     end
-  | succ2 _ H e2 =>
+  | lessThan2 _ H e2 e3 =>
     match H in (_ = b) return exp1 b with
-    | eq_refl =>
-      succ1 (
-        exp2ToExp1
-          nat
-          match (eq_sym H) in (_ = b) return exp2 b with
-          | eq_refl => e2
-          end
-      )
-    end
-  | pair2 _ c d H e2 e3 =>
-    match H in (_ = b) return exp1 b with
-    | eq_refl =>
-      pair1 c d (exp2ToExp1 c e2) (exp2ToExp1 d e3)
+    | eq_refl => lessThan1 (exp2ToExp1 nat e2) (exp2ToExp1 nat e3)
     end
   end.
 
@@ -87,6 +69,10 @@ Theorem exp1ToExp2Homomorphism :
 Proof.
   clean.
   induction e; magic.
+  clean.
+  rewrite IHe1.
+  rewrite IHe2.
+  magic.
 Qed.
 
 #[export] Hint Resolve exp1ToExp2Homomorphism : main.
@@ -96,6 +82,10 @@ Theorem exp2ToExp1Homomorphism :
 Proof.
   clean.
   induction e; magic.
+  clean.
+  rewrite IHe1.
+  rewrite IHe2.
+  magic.
 Qed.
 
 #[export] Hint Resolve exp2ToExp1Homomorphism : main.
