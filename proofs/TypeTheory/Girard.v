26737e85e373526ad6248896af68fc376cdc980d
Stephan Boyer <stephan@stephanboyer.com>
2022-05-25 07:32:27 -0700

    Tweak some comments

--- proofs/TypeTheory/Girard.v
+++ proofs/TypeTheory/Girard.v
@@ -10,7 +10,7 @@
   Girard's paradox shows that any type is inhabited if `Type : Type`. The
   presentation of the paradox was adapted from this paper:
 
-    [1] Antonius J. C. Hurkens. 1995. A Simplification of Girard's Paradox. In
+    Antonius J. C. Hurkens. 1995. A Simplification of Girard's Paradox. In
     Proceedings of the Second International Conference on Typed Lambda Calculi
     and Applications (TLCA '95). Springer-Verlag, Berlin, Heidelberg, 266-278.
 
