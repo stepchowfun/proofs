7e93bf130e460329fa6853f5a5c204b5fdcd93b5
Stephan Boyer <stephan@stephanboyer.com>
2022-05-26 06:26:19 -0700

    Explain the universe constraints related to inductive types

--- proofs/Tutorial/Lesson5_Consistency.v
+++ proofs/Tutorial/Lesson5_Consistency.v
@@ -107,6 +107,56 @@ Definition idUniverse (t : universe) (x : t) := x.
   ```
 *)
 
+(*********************************)
+(* Universes and inductive types *)
+(*********************************)
+
+(* Let's define a universe larger than `Set`. *)
+
+Definition large := Type.
+
+Definition constraint : large := Set.
+
+(*
+  An inductive type must either live in `Prop` or in a higher universe than the
+  universes of the arguments to its constructors, including indices but not
+  parameters.
+*)
+
+Inductive foo1 : Set :=
+| makeFoo1 : forall (x : nat), foo1.
+
+(*
+  Inductive foo2 : Set :=
+  | makeFoo2 : forall (x : Set), foo2.
+
+  Inductive foo3 : Set -> Set :=
+  | makeFoo3 : forall (x : Set), foo3 x.
+*)
+
+Inductive foo3 : Prop :=
+| makeFoo3 : forall (x : Set), foo3.
+
+Inductive foo4 : Set -> Prop :=
+| makeFoo4 : forall (x : Set), foo4 x.
+
+Inductive foo5 : Prop :=
+| makeFoo5 : forall (x : large), foo5.
+
+Inductive foo6 : large -> Prop :=
+| makeFoo6 : forall (x : large), foo6 x.
+
+(*
+  There's no constraint between the universe of an inductive type and the
+  universes of its parameters.
+*)
+
+Inductive foo7 (x : Set) : Set :=
+| makeFoo7 : foo7 x.
+
+Inductive foo8 (x : large) : Set :=
+| makeFoo8 : foo8 x.
+
 (***********************************************************)
 (* Inductive types have a "strict positivity requirement". *)
 (***********************************************************)
