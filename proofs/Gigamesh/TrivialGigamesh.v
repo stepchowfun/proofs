947be88ae163545f40f7da2aaffe82d2cfe26dcb
Stephan Boyer <stephan@stephanboyer.com>
2023-03-09 06:16:42 -0800

    Allow gigamesh nodes to have multiple parents and reformulate the encapsulation axiom

--- proofs/Gigamesh/TrivialGigamesh.v
+++ proofs/Gigamesh/TrivialGigamesh.v
@@ -15,34 +15,22 @@ Module TrivialGigamesh <: Gigamesh.
   #[local] Arguments clos_refl_trans {A} _ _ _.
   #[local] Hint Resolve rt_refl : main.
 
-
   Definition node := unit.
 
   #[export] Hint Unfold node : main.
-
-  Definition root := tt.
-
-  #[export] Hint Unfold root : main.
 
   Definition edge (n1 n2 : node) := False.
 
   #[export] Hint Unfold edge : main.
 
-  Definition parent (n : node) := tt.
+  Definition parent (n1 : node) (n2 : node) := False.
 
   #[export] Hint Unfold parent : main.
 
   (* Coq requires that we copy this verbatim from `Gigamesh`. *)
-  Definition ancestor := clos_refl_trans (fun n1 n2 => n1 = parent n2).
+  Definition ancestor := clos_refl_trans parent.
 
   #[export] Hint Unfold ancestor : main.
-
-  Theorem rootedness : forall n, ancestor root n.
-  Proof.
-    search.
-  Qed.
-
-  #[export] Hint Resolve rootedness : main.
 
   Theorem antisymmetry :
     forall n1 n2, ancestor n1 n2 -> ancestor n2 n1 -> n1 = n2.
@@ -52,22 +40,18 @@ Module TrivialGigamesh <: Gigamesh.
 
   #[export] Hint Resolve antisymmetry : main.
 
-  Theorem connectedness :
-    forall n,
-    let p := parent n
-    in clos_refl_trans (fun n1 n2 => edge n1 n2 /\ ancestor p n2) p n.
+  (* Coq requires that we copy this verbatim from `Gigamesh`. *)
+  Definition admitted n1 n2 :=
+    exists n3 n4, ancestor n3 n1 /\ parent n3 n4 /\ ancestor n2 n4.
+
+  #[export] Hint Unfold admitted : main.
+
+  Theorem admission : forall n1 n2, edge n1 n2 -> admitted n1 n2.
   Proof.
     search.
   Qed.
 
-  #[export] Hint Resolve connectedness : main.
-
-  Theorem encapsulation : forall n1 n2, edge n1 n2 -> ancestor (parent n2) n1.
-  Proof.
-    search.
-  Qed.
-
-  #[export] Hint Resolve encapsulation : main.
+  #[export] Hint Resolve admission : main.
 End TrivialGigamesh.
 
 Module TrivialGigameshTheorems := GigameshTheorems TrivialGigamesh.
