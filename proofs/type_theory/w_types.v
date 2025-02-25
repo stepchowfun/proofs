701581818e5f579999daa2229de160f62e2368b6
Stephan Boyer <stephan@stephanboyer.com>
2025-02-25 07:58:00 -0500

    Implement the W-encoded natural number type with a dependent eliminator

--- proofs/type_theory/w_types.v
+++ proofs/type_theory/w_types.v
@@ -41,13 +41,14 @@ Check W_ind.
 
 Definition arities (b : bool) := if b then Empty_set else unit.
 
-Definition Nat := W arities.
+Definition PreNat := W arities.
 
-Definition zero : Nat := sup true (fun x : Empty_set => match x with end).
+Definition pre_zero : PreNat :=
+  sup true (fun x : Empty_set => match x with end).
 
-Definition succ (p : Nat) : Nat := sup false (fun _ => p).
+Definition pre_succ (p : PreNat) : PreNat := sup false (fun _ => p).
 
-Definition recursor P (p_zero : P) (p_succ : P -> P) : Nat -> P :=
+Definition pre_recursor P (p_zero : P) (p_succ : P -> P) : PreNat -> P :=
   W_rect bool arities (fun _ => P) (
     fun (a : bool) =>
       match a
@@ -58,21 +59,23 @@ Definition recursor P (p_zero : P) (p_succ : P -> P) : Nat -> P :=
       end
   ).
 
-Definition add m n := recursor Nat n succ m.
+Definition pre_add m n := pre_recursor PreNat n pre_succ m.
 
-Definition to_built_in_nat n := recursor nat 0 S n.
+Definition pre_nat_to_standard_nat n := pre_recursor nat 0 S n.
 
-Compute to_built_in_nat (add (succ zero) (succ (succ zero))). (* `3` *)
+Compute pre_nat_to_standard_nat (
+  pre_add (pre_succ pre_zero) (pre_succ (pre_succ pre_zero))
+). (* `3` *)
 
 (*
   Unfortunately, we need function extensionality to define the dependent
-  eliminator for this encoding of `Nat`.
+  eliminator for `PreNat`.
 *)
 
-Definition eliminator
-  (P : Nat -> Type)
-  (p_zero : P zero)
-  (p_succ : forall n, P n -> P (succ n))
+Definition pre_eliminator
+  (P : PreNat -> Type)
+  (p_zero : P pre_zero)
+  (p_succ : forall n, P n -> P (pre_succ n))
 : forall n, P n
 :=
   W_rect bool arities P (
@@ -112,17 +115,17 @@ Definition eliminator
       end
   ).
 
-Goal forall n, add zero n = n.
+Goal forall n, pre_add pre_zero n = n.
 Proof.
   reflexivity.
 Qed.
 
-Goal forall n, add n zero = n.
+Goal forall n, pre_add n pre_zero = n.
 Proof.
-  apply eliminator; auto.
+  apply pre_eliminator; auto.
   intros.
   cbn.
-  unfold add in H.
+  unfold pre_add in H.
   rewrite H.
   reflexivity.
 Qed.
@@ -144,12 +147,12 @@ Qed.
   that is extensionally but not judgmentally equal to `zero`:
 *)
 
-Definition other_zero : Nat := sup true (fun _ : Empty_set => zero).
+Definition other_zero : PreNat := sup true (fun _ : Empty_set => pre_zero).
 
-Goal zero = other_zero.
+Goal pre_zero = other_zero.
 Proof.
   assert_fails reflexivity. (* Not judgmentally equal *)
-  unfold zero, other_zero.
+  unfold pre_zero, other_zero.
   f_equal.
   apply functional_extensionality.
   intro.
@@ -167,4 +170,118 @@ Qed.
     Informatics (LIPIcs), Volume 188, pp. 8:1-8:9, Schloss Dagstuhl –
     Leibniz-Zentrum für Informatik (2021)
     https://doi.org/10.4230/LIPIcs.TYPES.2020.8
+
+  The Σ type in Coq's standard library doesn't have definitional
+  η-conversion:
 *)
+
+Goal
+  forall T (P : T -> Type) (s : { x : T & P x }),
+  s = existT _ (projT1 s) (projT2 s).
+Proof.
+  intros.
+  assert_fails reflexivity. (* Conversion fails. *)
+  destruct s.
+  reflexivity.
+Qed.
+
+(*
+  However, we can define a Σ with η-conversion as a primitive record type:
+*)
+
+Record Sigma [T] (P : T -> Type) := sigma { x : T; y : P x }.
+
+Arguments sigma [_] _ _ _.
+Arguments x [_ _] _.
+Arguments y [_ _] _.
+
+Goal
+  forall T (P : T -> Type) (s : Sigma P),
+  s = {| x := s.(x); y := s.(y) |}.
+Proof.
+  intros.
+  reflexivity. (* Conversion succeeds. *)
+Qed.
+
+(* Now we can proceed to define `Nat`. *)
+
+Fixpoint Canonical (n : PreNat) : Type :=
+  match n with
+  | sup true f => (fun x : Empty_set => match x with end) = f
+  | sup false f =>
+    Sigma (fun k => Sigma (fun _ : (fun _ => k) = f => Canonical (f tt)))
+  end.
+
+Definition Nat := Sigma Canonical.
+
+Definition zero : Nat := {|
+  x := sup true (fun x : Empty_set => match x with end);
+  y := eq_refl;
+|}.
+
+Definition succ (n : Nat) : Nat := {|
+  x := sup false (fun _ => n.(x));
+  y := {| x := _; y := {| x := eq_refl; y := n.(y) |} |}
+|}.
+
+Definition eliminator
+  (P : Nat -> Type)
+  (p_zero : P zero)
+  (p_succ : forall n, P n -> P (succ n))
+  (n : Nat)
+: P n
+:=
+  let H n := forall c : Canonical n, P {| x := n; y := c |} in
+  W_rect bool arities H
+  (
+    fun (a : bool) =>
+      match a
+      return
+        forall (f : arities a -> W arities) (h : forall b, H (f b)),
+        H (sup a f)
+      with
+      | true => fun f _ c =>
+        match c in _ = z return P {| x := sup true z; y := c |} with
+        | eq_refl => p_zero
+        end
+      | false => fun f h c =>
+        let eq := c.(y).(x) in
+        match eq
+        in _ = z
+        return
+          forall p : Canonical (z tt),
+          P {| x := z tt; y := p |} ->
+          P {|
+            x := sup false z;
+            y := {| x := c.(x); y := {| x := eq; y := p |} |}
+          |}
+        with
+        | eq_refl => fun p ih => p_succ {| x := c.(x); y := p |} ih
+        end
+        c.(y).(y) (h tt c.(y).(y))
+      end
+  ) n.(x) n.(y).
+
+Definition recursor P (p_zero : P) (p_succ : P -> P) (n : Nat) : P :=
+  eliminator (fun _ => P) p_zero (fun _ x => p_succ x) n.
+
+Definition add m n := recursor Nat n succ m.
+
+Definition nat_to_standard_nat n := recursor nat 0 S n.
+
+Compute nat_to_standard_nat (add (succ zero) (succ (succ zero))). (* `3` *)
+
+Goal forall n, add zero n = n.
+Proof.
+  reflexivity.
+Qed.
+
+Goal forall n, add n zero = n.
+Proof.
+  apply eliminator; auto.
+  intros.
+  change (succ (recursor Nat zero succ n) = succ n).
+  unfold add in H.
+  rewrite H.
+  reflexivity.
+Qed.
