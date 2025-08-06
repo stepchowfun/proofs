8f5dea4330ba7545f90ab50ffbc704abf77280ee
Stephan Boyer <stephan@stephanboyer.com>
2025-08-06 06:36:25 -0700

    Clarify some universes in the HoTT development

--- proofs/type_theory/homotopy_type_theory.v
+++ proofs/type_theory/homotopy_type_theory.v
@@ -18,41 +18,41 @@ Definition U := Type.
 
 (* The groupoid structure of types *)
 
-Definition inv [T : U] [x y : T] (p : x = y) : y = x :=
+Definition inv [A] [x y : A] (p : x = y) : y = x :=
   match p in _ = z return z = x with
   | eq_refl => eq_refl
   end.
 
-Definition concat [T : U] [x y z : T] (p : x = y) (q : y = z) : x = z :=
+Definition concat [A] [x y z : A] (p : x = y) (q : y = z) : x = z :=
   match q in _ = z return x = z with
   | eq_refl => p
   end.
 
-Definition left_id [T : U] [x y : T] (p : x = y) : concat eq_refl p = p :=
+Definition left_id [A] [x y : A] (p : x = y) : concat eq_refl p = p :=
   match p return concat eq_refl p = p with
   | eq_refl => eq_refl
   end.
 
-Definition right_id [T : U] [x y : T] (p : x = y) : concat p eq_refl = p :=
+Definition right_id [A] [x y : A] (p : x = y) : concat p eq_refl = p :=
   match p return concat p eq_refl = p with
   | eq_refl => eq_refl
   end.
 
-Definition left_inv [T : U] [x y : T] (p : x = y) :
+Definition left_inv [A] [x y : A] (p : x = y) :
   concat (inv p) p = eq_refl
 :=
   match p return concat (inv p) p = eq_refl with
   | eq_refl => eq_refl
   end.
 
-Definition right_inv [T : U] [x y : T] (p : x = y) :
+Definition right_inv [A] [x y : A] (p : x = y) :
   concat p (inv p) = eq_refl
 :=
   match p return concat p (inv p) = eq_refl with
   | eq_refl => eq_refl
   end.
 
-Definition assoc [T : U] [x y z w : T]
+Definition assoc [A] [x y z w : A]
   (p : x = y) (q : y = z) (r : z = w)
 : concat (concat p q) r = concat p (concat q r)
 :=
@@ -65,43 +65,44 @@ Definition assoc [T : U] [x y z w : T]
 
 (* Transport *)
 
-Definition transport [A] [x y : A] [P : A -> Type] (p : x = y) (px : P x) :=
+Definition transport [A] [x y : A] [P : A -> Type] (p : x = y) (px : P x)
+:=
   match p in _ = z return P z with
   | eq_refl => px
   end.
 
 (* Homotopy *)
 
-Definition Homotopy [X] [Y : X -> Type] (f g : forall x : X, Y x) :=
+Definition Homotopy [A] [B : A -> Type] (f g : forall x : A, B x) :=
   forall x, f x = g x.
 
 (* Equivalence *)
 
-Definition IsEquiv [X Y] (f : X -> Y) :=
-  { g : Y -> X & Homotopy (f ∘ g) id } *
-  { g : Y -> X & Homotopy (g ∘ f) id }.
+Definition IsEquiv [A B] (f : A -> B) :=
+  { g : B -> A & Homotopy (f ∘ g) id } *
+  { g : B -> A & Homotopy (g ∘ f) id }.
 
 (* Quasi-inverse *)
 
-Definition QuasiInv [X Y] (f : X -> Y) :=
-  { g : Y -> X & Homotopy (f ∘ g) id * Homotopy (g ∘ f) id }.
+Definition QuasiInv [A B] (f : A -> B) :=
+  { g : B -> A & Homotopy (f ∘ g) id * Homotopy (g ∘ f) id }.
 
 (* Equivalence is logically equivalent to quasi-inverse. *)
 
 Theorem quasi_inverse_to_equiv :
-  forall X Y (f : X -> Y), QuasiInv f -> IsEquiv f.
+  forall A B (f : A -> B), QuasiInv f -> IsEquiv f.
 Proof.
   intros.
-  destruct X0, p.
+  destruct X, p.
   split; exists x; auto.
 Qed.
 
 Theorem equiv_to_quasi_inverse :
-  forall X Y (f : X -> Y), IsEquiv f -> QuasiInv f.
+  forall A B (f : A -> B), IsEquiv f -> QuasiInv f.
 Proof.
   unfold IsEquiv, QuasiInv, Homotopy, compose, id.
   intros.
-  destruct X0, s, s0.
+  destruct X, s, s0.
   exists (fun y => x0 (f (x y))).
   split; intros.
   - rewrite e0.
@@ -112,8 +113,8 @@ Qed.
 
 (* Paths between maps can be converted to homotopies. *)
 
-Definition path_to_homotopy [X] [Y : X -> Type]
-  (f g : forall x : X, Y x) (p : f = g) :
+Definition path_to_homotopy [A] [B : A -> Type]
+  (f g : forall x : A, B x) (p : f = g) :
   Homotopy f g :=
   fun x =>
     match p in _ = h return f x = h x with
@@ -123,14 +124,14 @@ Definition path_to_homotopy [X] [Y : X -> Type]
 (* Function extensionality *)
 
 Axiom function_extensionality :
-  forall (X : U) (Y : X -> U) (f g : forall x : X, Y x),
+  forall (A : U) (B : A -> U) (f g : forall x : A, B x),
   IsEquiv (path_to_homotopy f g).
 
 (* Paths can be converted to equivalences. *)
 
-Definition path_to_equiv [X Y] (p : X = Y) :
-  { f : X -> Y & IsEquiv f } :=
-  existT (@IsEquiv X Y) (transport p)
+Definition path_to_equiv [A B] (p : A = B) :
+  { f : A -> B & IsEquiv f } :=
+  existT (@IsEquiv A B) (transport p)
     match p with
     | eq_refl => (
         existT (fun g => Homotopy g _) _ (@eq_refl _),
@@ -140,14 +141,14 @@ Definition path_to_equiv [X Y] (p : X = Y) :
 
 (* Univalence *)
 
-Axiom univalence : forall (X Y : U), IsEquiv (@path_to_equiv X Y).
+Axiom univalence : forall A B : U, IsEquiv (@path_to_equiv A B).
 
 (* Homotopy n-types (starting at 0 rather than the more conventional -2) *)
 
-Fixpoint IsTrunc n (X : U) : U :=
+Fixpoint IsTrunc n (A : Type) :=
   match n with
-  | O => { c : X & forall x, c = x }
-  | S p => forall x y : X, IsTrunc p (x = y)
+  | O => { c : A & forall x, c = x }
+  | S p => forall x y : A, IsTrunc p (x = y)
   end.
 
 (* Contractible types, a.k.a. homotopy (-2)-types or (-2)-truncated spaces *)
@@ -164,13 +165,13 @@ Definition IsSet := IsTrunc 2.
 
 (* `IsTrunc` defines a filtration on the universe. *)
 
-Theorem is_trunc_cumulative : forall n X, IsTrunc n X -> IsTrunc (1 + n) X.
+Theorem is_trunc_cumulative : forall n A, IsTrunc n A -> IsTrunc (1 + n) A.
 Proof.
   induction n.
   - unfold IsTrunc.
     cbn.
     intros.
-    destruct X0.
+    destruct X.
     exists (eq_trans (eq_sym (e x)) (e y)).
     intro.
     destruct x1, (e x).
@@ -178,12 +179,12 @@ Proof.
   - cbn in *.
     intros.
     apply IHn.
-    apply X0.
+    apply X.
 Qed.
 
 (* An equivalent characterization of being a mere proposition *)
 
-Theorem proof_irrelevance_is_prop X : (forall x y : X, x = y) -> IsProp X.
+Theorem proof_irrelevance_is_prop A : (forall x y : A, x = y) -> IsProp A.
 Proof.
   unfold IsProp, IsTrunc.
   intros.
@@ -196,7 +197,7 @@ Proof.
     reflexivity.
   - specialize (H0 _ _ (H x x)).
     assert (
-      forall (x y z : X) (p : x = y) (q r : y = z),
+      forall (x y z : A) (p : x = y) (q r : y = z),
       concat p q = concat p r -> q = r
     ).
     + intros.
@@ -209,14 +210,14 @@ Qed.
 
 (* Being truncated is a mere proposition. *)
 
-Theorem is_truncated_is_prop : forall n X, IsProp (IsTrunc n X).
+Theorem is_truncated_is_prop : forall n A, IsProp (IsTrunc n A).
 Proof.
   induction n; intros; apply proof_irrelevance_is_prop; intros.
   - unfold IsTrunc in *.
     destruct x, y.
     destruct (e0 x).
     assert (e = e0).
-    + assert (IsProp X).
+    + assert (IsProp A).
       * apply proof_irrelevance_is_prop.
         intros.
         rewrite <- (e x).
@@ -227,8 +228,8 @@ Proof.
         apply x.
         unfold Homotopy.
         intro.
-        pose proof (is_trunc_cumulative 1 X X0).
-        destruct (X1 x0 x1 (e x1) (e0 x1)).
+        pose proof (is_trunc_cumulative 1 A X).
+        destruct (X0 x0 x1 (e x1) (e0 x1)).
         auto.
     + rewrite H.
       reflexivity.
@@ -267,31 +268,15 @@ Definition bit_to_weekend x :=
   | One => Sunday
   end.
 
-Definition weekend_bit_equiv : IsEquiv weekend_to_bit := (
-  existT (fun g => Homotopy (weekend_to_bit ∘ g) id)
-    bit_to_weekend
-    (
-      fun x =>
-        match x with
-        | Zero => eq_refl _
-        | One => eq_refl _
-        end
-    ),
-  existT (fun g => Homotopy (g ∘ weekend_to_bit) id)
-    bit_to_weekend
-    (
-      fun x =>
-        match x with
-        | Saturday => eq_refl _
-        | Sunday => eq_refl _
-        end
-    )
-).
+Theorem weekend_bit_equiv : IsEquiv weekend_to_bit.
+Proof.
+  apply quasi_inverse_to_equiv.
+  exists bit_to_weekend.
+  split; intro; destruct x; reflexivity.
+Qed.
 
 Definition weekend_bit_path : Weekend = Bit :=
-  projT1
-    (fst (univalence Weekend Bit))
-    (existT _ weekend_to_bit weekend_bit_equiv).
+  projT1 (fst (univalence _ _)) (existT _ _ weekend_bit_equiv).
 
 Definition invert_weekend x :=
   match x with
@@ -310,8 +295,8 @@ Definition invert_weekend_with_theorem :=
     invert_weekend_involution.
 
 Definition invert_bit_with_theorem :=
-  match weekend_bit_path in _ = Z
-  return { invert : Z -> Z | forall x, invert (invert x) = x } with
+  match weekend_bit_path in _ = A
+  return { invert : A -> A | forall x, invert (invert x) = x } with
   | eq_refl _ => invert_weekend_with_theorem
   end.
 
